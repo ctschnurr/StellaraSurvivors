801c2df9f84c381ffb5a26612816596542826536
ctschnurr <ctschnurr@hotmail.com>
2024-05-03 07:33:44 -0300

    Tweaked asteroid collision system, sometimes the bounces don't look right, will need a little more work

--- scenes/asteroids/asteroid.gd
+++ scenes/asteroids/asteroid.gd
@@ -10,6 +10,8 @@ var asteroid_direction
 var x_movement
 var y_movement
 
+var has_hit:bool = false
+var was_hit:bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	asteroid_rotation = randf_range(-0.025, 0.025)
@@ -37,16 +39,19 @@ func fire_raycast(direction: Vector2):
 	var space_state = get_world_2d().direct_space_state
 	var raycast = PhysicsRayQueryParameters2D.create(global_position, global_position + (direction * 15))
 	raycast.exclude = [self]
+	
 	var raycast_output = space_state.intersect_ray(raycast)
 	
 	if !raycast_output.is_empty():
+		has_hit = true
 		var collision_normal: Vector2 = raycast_output.normal
-		var new_direction = asteroid_direction.reflect(collision_normal).normalized()
+		var new_direction = direction.reflect(collision_normal).normalized()
 		
 		if raycast_output.collider is Asteroid:
 			raycast_output.collider.asteroid_direction = asteroid_direction.normalized() * 0.8
 			raycast_output.collider.asteroid_speed = asteroid_speed * 0.8
 			raycast_output.collider.collision_cooldown()
+			raycast_output.collider.was_hit = true
 			
 		asteroid_direction = new_direction
 		collision_cooldown()
@@ -56,15 +61,17 @@ func collision_cooldown():
 	set_physics_process(false)
 	await get_tree().create_timer(1).timeout
 	set_physics_process(true)
+	has_hit = false
+	was_hit = false
 	
 	
 #func _draw():
-	#var look_left = asteroid_direction.rotated(-0.8)
-	#var look_right = asteroid_direction.rotated(0.8)
+#	var look_left = asteroid_direction.rotated(-0.75)
+#	var look_right = asteroid_direction.rotated(0.75)
 		
-	#draw_line(Vector2.ZERO, asteroid_direction * 20, Color.GREEN)
-	#draw_line(Vector2.ZERO, look_left * 20, Color.GREEN)
-	#draw_line(Vector2.ZERO, look_right * 20, Color.GREEN)
+#	draw_line(Vector2.ZERO, (Vector2.ZERO + asteroid_direction) * 20, Color.GREEN if !has_hit else Color.RED)
+#	draw_line(Vector2.ZERO, look_left * 20, Color.GREEN if !has_hit else Color.RED)
+#	draw_line(Vector2.ZERO, look_right * 20, Color.GREEN if !has_hit else Color.RED)
 
 		
 
