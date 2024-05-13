f085562c774b15505c0ec00811b08a27d45911bc
ctschnurr <ctschnurr@hotmail.com>
2024-05-13 08:15:51 -0300

    did some fine tuning on asteroid collision, added additional raycasts as they weren't always catching collisions properly

--- scenes/asteroids/asteroid.gd
+++ scenes/asteroids/asteroid.gd
@@ -22,8 +22,8 @@ func _ready():
 	enemy_sprite = %Asteroid_sprite
 	#health_component = %HealthComponent
 	
-	asteroid_rotation = randf_range(-0.025, 0.025)
-	asteroid_speed = randf_range(100, 150)
+	asteroid_rotation = randf_range(-0.01, 0.01) #randf_range(-0.025, 0.025)
+	asteroid_speed = 150 #randf_range(100, 150)
 	asteroid_direction = set_movement_vector()
 	
 	enemy_sprite.texture = asteroid_textures.pick_random()
@@ -49,12 +49,16 @@ func _process(_delta):
 func _physics_process(_delta):
 	queue_redraw()
 	
-	var raycast_left_direction = asteroid_direction.rotated(-0.8)
-	var raycast_right_direction = asteroid_direction.rotated(0.8)
+	var raycast_left_directionA = asteroid_direction.rotated(-1)
+	var raycast_left_directionB = asteroid_direction.rotated(-0.6)
+	var raycast_right_directionA = asteroid_direction.rotated(0.6)
+	var raycast_right_directionB = asteroid_direction.rotated(1)
 	
-	fire_raycast(raycast_left_direction)
+	fire_raycast(raycast_left_directionA)
+	fire_raycast(raycast_left_directionB)
 	fire_raycast(asteroid_direction)
-	fire_raycast(raycast_right_direction)
+	fire_raycast(raycast_right_directionA)
+	fire_raycast(raycast_right_directionB)
 		
 		
 func fire_raycast(direction: Vector2):
@@ -68,14 +72,18 @@ func fire_raycast(direction: Vector2):
 		has_hit = true
 		var collision_normal: Vector2 = raycast_output.normal
 		var new_direction = collision_normal
-		var new_speed = asteroid_speed * 0.75 / size_mult
+		var new_speed = asteroid_speed * 0.75
 		
 		if raycast_output.collider is Asteroid and !raycast_output.collider is Blaster_bolt:
-			new_speed = raycast_output.collider.asteroid_speed * 0.75
+			var new_rotation = raycast_output.collider.asteroid_rotation * 0.8
+			raycast_output.collider.asteroid_rotation = asteroid_rotation * 0.8
+			new_speed = raycast_output.collider.asteroid_speed * (0.3 * size_mult)
 			raycast_output.collider.asteroid_direction = direction.normalized()
-			if asteroid_speed > 25: raycast_output.collider.asteroid_speed = asteroid_speed * 0.75 / size_mult
+			if asteroid_speed > 25: raycast_output.collider.asteroid_speed = asteroid_speed * 0.75
 			raycast_output.collider.collision_cooldown()
 			raycast_output.collider.was_hit = true
+			
+			asteroid_rotation = new_rotation
 		
 		if new_speed > 25: asteroid_speed = new_speed
 		asteroid_direction = new_direction
@@ -84,7 +92,7 @@ func fire_raycast(direction: Vector2):
 
 func collision_cooldown():
 	set_physics_process(false)
-	await get_tree().create_timer(0.5).timeout
+	await get_tree().create_timer(0.25).timeout
 	set_physics_process(true)
 	has_hit = false
 	was_hit = false
@@ -98,9 +106,9 @@ func respond_to_bolt_collision(bolt_direction):
 	if diff_angle < -2.75 or diff_angle > 2.75: 
 		if asteroid_speed < 5:
 			asteroid_direction = bolt_direction.normalized()
-			asteroid_speed *= 1.5
+			asteroid_speed *= 1.5 
 		else:
-			asteroid_speed *= 0.5
+			asteroid_speed *= 0.3 * size_mult
 					
 	#if the bolt hits an asteroid traveling away from it:
 	if diff_angle > -0.5 and diff_angle < 0.5: 
